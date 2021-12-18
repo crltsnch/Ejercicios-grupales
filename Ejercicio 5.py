4786b812191b2fa85f944b47afee28d3eb9e7a81
crltsnch <crltsnch@myuax.com>
2021-12-18 18:40:15 +0100

    Ejercicio 5: Crear una función que diga quien gana

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -12,11 +12,19 @@ import sys
 #
 def gameOfStones(n):
 # Write your code here
-    if __name__ == '__main__':
-        fptr = open(os.environ['OUTPUT_PATH'], 'w')
-        t = int(input().strip())
-        for t_itr in range(t):
-            n = int(input().strip())
-            result = gameOfStones(n)
-            fptr.write(result + '\n')
-        fptr.close()
+    ganador=""
+    if(jugadaoptima(n)!=0):
+        ganador="P1 is the winner"
+    else:
+        ganador ="P2 is the winner"
+    return ganador
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    t = int(input().strip())
+    for t_itr in range(t):
+        n = int(input().strip())
+        result = gameOfStones(n)
+        fptr.write(result + '\n')
+    fptr.close()
