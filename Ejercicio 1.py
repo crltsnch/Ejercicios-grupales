ae28ba80e3b24a7a8ca844e6e4119ce7b1643a2d
crltsnch <crltsnch@myuax.com>
2021-12-18 18:09:53 +0100

    Ejercicio 1: Completar la funcion para que sume todos los elementos

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,21 +1,21 @@
-#!/bin/python3
+
 import math
 import os
 import random
 import re
 import sys
-#
-# Complete the 'simpleArraySum' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER_ARRAY ar as parameter.
-#
+
 def simpleArraySum(ar):
     # Write your code here
-    if __name__ == '__main__':
-        fptr = open(os.environ['OUTPUT_PATH'], 'w')
-        ar_count = int(input().strip())
-        ar = list(map(int, input().rstrip().split()))
-        result = simpleArraySum(ar)
-        fptr.write(str(result) + '\n')
-        fptr.close()
+    suma=0
+    for i in ar:
+        suma= suma+i
+    return suma
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    ar_count = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+    result = simpleArraySum(ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()
