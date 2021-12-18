d8a99c0a7fdcdb090526d72064123000f4dd5354
crltsnch <crltsnch@myuax.com>
2021-12-18 18:12:04 +0100

    Ejercicio 1: Que el usuario escrib ael tamaño y los numeros de la matriz y de el resultado

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -13,9 +13,14 @@ def simpleArraySum(ar):
     return suma
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = open(os.environ['OUTPUT_PATH'] + 'solucion1.txt', 'w')
+    print("Escribe el tamaño")
     ar_count = int(input().strip())
+    print("Escribe los números separados por espacio")
     ar = list(map(int, input().rstrip().split()))
+
     result = simpleArraySum(ar)
+
     fptr.write(str(result) + '\n')
+
     fptr.close()
