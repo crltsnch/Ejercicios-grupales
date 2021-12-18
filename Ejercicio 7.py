8f17ef18035622eb5b9b8f76206f0b16ab13fe52
crltsnch <crltsnch@myuax.com>
2021-12-18 19:21:37 +0100

    Ejercicio 7: Completar

--- Ejercicio 7.py
+++ Ejercicio 7.py
@@ -23,3 +23,22 @@ def finalgrade(grades):
         else:
             roundgrade=grades
     return roundgrade
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'] + "solucion7.txt", 'w')
+    print("Número de estudiantes")
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        print("Nota de cada estudiante")
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
