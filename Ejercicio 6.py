aac89e42df41a970b5824227259234e810aa5319
crltsnch <crltsnch@myuax.com>
2021-12-18 18:56:39 +0100

    Ejercicio 6: función que determine los tuneles

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -13,7 +13,24 @@ class Coordenadas:
             return True
         else:
             return False
-        
+
+class Tunel:
+    def __init__(self, x1, y1, x2, y2):
+        self.extremo1 = Coordenadas(x1, y1)
+        self.extremo2 = Coordenadas(x2, y2)
+def buscaTunel(Casillax,Casillay,tuneles):
+    coordenadas = Coordenadas(Casillax, Casillay)
+    for t in tuneles:
+        if(t.extremo1.comparate(Casillax,Casillay)==True):
+            coordenadas.x=t.extremo2.x
+            coordenadas.y=t.extremo2.y
+            break
+        elif(t.extremo2.comparate(Casillax,Casillay)==True):
+            coordenadas.x=t.extremo1.x
+            coordenadas.y=t.extremo1.y
+            break
+    return coordenadas
+
 
 if __name__ == '__main__':
     first_multiple_input = input().rstrip().split()
